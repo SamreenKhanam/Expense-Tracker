9aff11a957993668f13e62e348e1162b237cf53f
SamreenKhanam <95101539+SamreenKhanam@users.noreply.github.com>
2024-04-19 19:56:50 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+const balance = document.getElementById(
+    "balance"
+);
+const money_plus = document.getElementById('money-plus');
+const money_minus = document.getElementById('money-minus');
+const list = document.getElementById("list");
+const form = document.getElementById("form");
+const text = document.getElementById("text");
+const amount = document.getElementById("amount");
+
+const dummyTransactions = [
+    {id: 1, text: "Flower" , amount:-20},
+    {id: 2, text: "Salary" , amount:-300},
+    {id: 3, text: "Salary" , amount:-10},
+    {id: 4, text: "Salary" , amount:-150},
+];
+let Transactions = dummyTransactions;
+
